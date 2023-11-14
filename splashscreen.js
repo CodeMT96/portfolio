a446874b996b6f4482fc378b443acd57cbfe7245
markus <markusteumel@gmail.com>
2023-11-14 15:52:54 +0100

    last commit for day 2

--- splashscreen.js
+++ splashscreen.js
@@ -1,14 +1,26 @@
-// Das ist die delcaration die meinem script sagt das mein div mit der "screencover" id für ihn splashScreen heißt 
+// Das ist die delcaration die meinem script sagt das mein div mit der "screencover" id für ihn splashScreen heißt
 const splashScreen = document.getElementById("screencover");
 // Hier sind noch mehr declarations
 const splashScreenText = document.getElementById("splashText");
-const splashScreenDivs = document.getElementById("splashanim")
+const splashScreenDivs = document.getElementById("splashanim");
 // Notes wenn ich erklären muss
-// Die function added eine klasse zu dem splashScreen an dem die Animation fadeOut gebunden ist 
+// Die function added eine klasse zu dem splashScreen an dem die Animation fadeOut gebunden ist
 function clickToView() {
-    splashScreen.classList.add("fadeout")
-    splashScreenText.classList.add("fadeoutFast")
-    splashScreenDivs.classList.add("fadeoutFast")
+  splashScreen.classList.add("fadeout");
+  splashScreenText.classList.add("fadeoutFast");
+  splashScreenDivs.classList.add("fadeoutFast");
 }
-// Das ist der Eventlistener der listend für events duhhh 
+// Eine Function um die opacity des splashScreens zu checken
+function hideSplashScreen() {
+  var computedStyle = window.getComputedStyle(splashScreen);
+  var opacity = computedStyle.getPropertyValue("opacity");
+  console.log("opacity:", opacity);
+  if (opacity === 0) {
+    splashScreen.style.display = "none";
+  }
+}
+hideSplashScreen();
+setTimeout(hideSplashScreen,2550);
+
+// Das ist der Eventlistener der listend für events duhhh
 splashScreen.addEventListener("click", clickToView);
