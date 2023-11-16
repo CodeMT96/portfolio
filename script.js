951ad575fd2edca04a8e58c0a9db127448b16b3d
markus <markusteumel@gmail.com>
2023-11-16 13:47:15 +0100

    added header stylings

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
+// Notes wenn ich erklären muss
 // Das ist die delcaration die meinem script sagt das mein div mit der "screencover" id für ihn splashScreen heißt
 const splashScreen = document.getElementById("screencover");
 // Hier sind noch mehr declarations
 const splashScreenText = document.getElementById("splashText");
 const splashScreenDivs = document.getElementById("splashanim");
-// Notes wenn ich erklären muss
+const htmlBody = document.getElementById("forScroll")
+
+function disableScrolling() {
+  htmlBody.classList.add("stopScroll")
+}
+disableScrolling()
 // Die function added eine klasse zu dem splashScreen an dem die Animation fadeOut gebunden ist
 function clickToView() {
   splashScreen.classList.add("fadeout");
   splashScreenText.classList.add("fadeoutFast");
   splashScreenDivs.classList.add("fadeoutFast");
-}
-// Eine Function um die opacity des splashScreens zu checken
-function hideSplashScreen() {
-  var computedStyle = window.getComputedStyle(splashScreen);
-  var opacity = computedStyle.getPropertyValue("opacity");
-  console.log("opacity:", opacity);
-  if (opacity === 0) {
+  setTimeout(() => {
     splashScreen.style.display = "none";
-  }
+    htmlBody.classList.remove("stopScroll")
+  }, 2500)
 }
-hideSplashScreen();
-setTimeout(hideSplashScreen,2550);
+
 
 // Das ist der Eventlistener der listend für events duhhh
 splashScreen.addEventListener("click", clickToView);
+
